269d132e721e0c5219dd2babcbc9b38ba668f2d8
David Sampson <Davidchosen33@gmail.com>
2025-05-04 19:08:47 +0100

    updated

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const Product = require("./productModel")
+
 
 const app = express()
 
@@ -74,97 +76,182 @@ app.get("/", (req, res)=>{
 })
 
 
-app.get("/drugs/antibiotics", (req, res)=>{
+// app.get("/drugs/antibiotics", (req, res)=>{
 
-    const antibiotics = drugs.filter((each)=>{
-        return each.category == "Antibiotic"
-    })
+//     const antibiotics = drugs.filter((each)=>{
+//         return each.category == "Antibiotic"
+//     })
 
-    res.json(antibiotics)
+//     res.json(antibiotics)
 
-})
+// })
 
-app.get("/drugs/names", (req, res)=>{
-    const drugNames = drugs.map((each)=>{
-        return each.name.toLowerCase()
-    })
+// app.get("/drugs/names", (req, res)=>{
+//     const drugNames = drugs.map((each)=>{
+//         return each.name.toLowerCase()
+//     })
 
-    res.json(drugNames)
+//     res.json(drugNames)
 
-})
+// })
 
-app.post("/drugs/by-category", (req, res)=>{
-    // const category = req.body.category
+// app.post("/drugs/by-category", (req, res)=>{
+//     // const category = req.body.category
 
-    const { category } = req.body
+//     const { category } = req.body
 
-    if(!category){
-        return res.json({mesage: "Please enter a category..."})
-    }
+//     if(!category){
+//         return res.json({mesage: "Please enter a category..."})
+//     }
 
-    const drugCategorized = drugs.filter((each)=>{
-        return each.category == category
-    })
+//     const drugCategorized = drugs.filter((each)=>{
+//         return each.category == category
+//     })
 
-    res.json(drugCategorized)
-})
+//     res.json(drugCategorized)
+// })
 
-app.get("/drugs/names-manufacturers", (req, res)=>{
-  const drugNamesAndManufactures = drugs.map((each)=>{
-    return {
-        drugName: each.name,
-        manufacturer: each.manufacturer
-    }
-  })
+// app.get("/drugs/names-manufacturers", (req, res)=>{
+//   const drugNamesAndManufactures = drugs.map((each)=>{
+//     return {
+//         drugName: each.name,
+//         manufacturer: each.manufacturer
+//     }
+//   })
 
-  res.json(drugNamesAndManufactures)
-})
+//   res.json(drugNamesAndManufactures)
+// })
 
-app.get("/drugs/prescription", (req, res)=>{
-    const prescribedDrug = drugs.filter((each)=>{
-        return each.isPrescriptionOnly == true // false
-    })
+// app.get("/drugs/prescription", (req, res)=>{
+//     const prescribedDrug = drugs.filter((each)=>{
+//         return each.isPrescriptionOnly == true // false
+//     })
 
-    res.json(prescribedDrug)
-})
+//     res.json(prescribedDrug)
+// })
 
 
-app.post("/drugs/manufacturer-count", (req, res)=>{
+// app.post("/drugs/manufacturer-count", (req, res)=>{
 
-    const { manufacturer } = req.body
+//     const { manufacturer } = req.body
 
-    if(!manufacturer){
-        return res.status(400).json({message: "Please add a manufacture"})
-    }
+//     if(!manufacturer){
+//         return res.status(400).json({message: "Please add a manufacture"})
+//     }
 
-    const manufacturerCount = drugs.filter((each)=>{
-        return each.manufacturer == manufacturer
-    })
+//     const manufacturerCount = drugs.filter((each)=>{
+//         return each.manufacturer == manufacturer
+//     })
+
+//     res.status(200).json({
+//         message: "success",
+//         count: manufacturerCount.length, 
+//         manufacturerCount
+//     })
+// })
+
+// aysnc  asynchronous      a synchronous
+// await
+
+// C R U D
+
+
+app.get("/all-product", async (req, res)=>{
+
+    const allProducts =  await Product.find()
 
     res.status(200).json({
-        message: "success",
-        count: manufacturerCount.length, 
-        manufacturerCount
+        message: "Succcess",
+        allProducts
+    })
+
+})
+
+
+
+app.post("/create-product", async (req, res)=>{
+
+    const { inStock, name, price, image, quantity  } = req.body
+
+    if(!name || !price){
+        return res.status(400).json({message: "Please enter all fields."})
+    }
+
+    const newProduct = new Product({ name, price, image, quantity, inStock })
+
+    await newProduct.save()
+
+    res.status(201).json({
+        message: "Success",
+        newProduct
     })
 })
 
-// Status Codes
-// 100 -------
-// 200, 201
-// 300, 301
-// 400, 401, 404
-// 500
+app.get("/one-product/:id", async (req, res)=>{
 
-// C R U D  
+    const { id } = req.params
 
-// CREATE
-// READ
-// UPDATE
-// DELETE
+    const product = await Product.findById(id)
 
-// AUTHENTICATION
+    if(!product){
+        return res.status(404).json({message: "Product not found."})
+    }
+
+    res.status(200).json({
+        message: "Success",
+        product
+    })
+
+})
+
+app.put("/edit-product/:id", async (req, res)=>{
+
+    const { id } = req.params
+
+    const { inStock, name, price, image, quantity } = req.body
+
+    const updatedProduct = await Product.findByIdAndUpdate(
+        id,
+        { inStock, name, price, image, quantity },
+        {new: true}
+    )
 
 
-// Relational 
+    res.status(201).json({
+        message: "Succcess",
+        updatedProduct
+    })
 
-// Non-Relational
+})
+
+
+app.patch("/update-product/:id", async (req, res)=>{
+
+    const { id } = req.params
+
+    const { name } = req.body
+
+    const existingProduct = await Product.findById(id)
+
+    if(existingProduct){
+        existingProduct.name = name
+
+        await existingProduct.save()
+
+        return res.status(200).json({
+            message: "Success",
+            existingProduct
+        })
+    } else {
+        res.status(404).json({ message: "Product not found" })
+    }
+})
+
+app.delete("/delete-product", async (req, res)=>{
+
+    const { id } = req.body
+
+    const deletedProduct = await Product.findByIdAndDelete(id)
+
+    res.status(200).json({message: "Success"})
+})
